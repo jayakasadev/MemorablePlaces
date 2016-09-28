b91113e564dfd44dc00a15355cce4f5ddd79adec
Jaya Kasa <kasa288@gmail.com>
2016-09-27 20:24:56 -0400

    Initial commit

---
+++ app/src/main/java/utils/prac/kasa/memorableplaces/MainActivity.java
@@ -0,0 +1,47 @@
+package utils.prac.kasa.memorableplaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.list);
+
+        final ArrayList<String> list = new ArrayList<>();
+
+        list.add("Click to add a new place");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String item = list.get(position);
+
+                Log.i("item", item);
+
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                i.putExtra("location", position);
+            }
+        });
+
+    }
+}
